6804d8e657c885afa9cbcdf07603acd70d829bad
Vivek Pandey <50174023+VivekPandey09032002@users.noreply.github.com>
2023-06-25 13:18:38 +0530

    face dectection

--- backend/src/main/java/com/microsoft/hackthon/controller/UserController.java
+++ backend/src/main/java/com/microsoft/hackthon/controller/UserController.java
@@ -15,11 +15,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 //import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.ErrorResponse;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
-@CrossOrigin(origins = "*", allowedHeaders = "*", exposedHeaders = "*")
 public class UserController {
 
     @Autowired
