219661f83d21c80631212d97c56f258acc1b3225
Amanjot Singh <amanjots726@gmail.com>
2023-06-25 09:52:55 +0530

    Updated

--- frontend/assets/js/stores.js
+++ frontend/assets/js/stores.js
@@ -1,42 +1,35 @@
 const server = "http://10.60.90.1:8080/api/v1"
 $(document).ready( function () {
-    $('#productTable').DataTable();
+    $('#storeTable').DataTable();
 });
 
 $(document).ready(function () {
 
-    getProducts();
+    getStores();
 
     function dataConverter(formData,index){
-        const {productId,productName,category,productPrice,rating,productQuantity,description} = formData
-        return [index,productName,category,productPrice,rating,productQuantity,description,`<button id="edit" data-id=${productId} title="Edit" class="btn btn-outline-primary rounded px-3 btn-sm"><i class="fas fa-pen"></i></button> <button id="delete"  data-id=${productId} title="Delete" class="btn btn-outline-danger rounded px-3 btn-sm"><i class="fas fa-trash"></i></button>`]
+        const {id,storeName,storeAddress} = formData
+        return [index,storeName,storeAddress,`<button id="edit" data-id="${id}" title="Edit" class="btn btn-outline-primary rounded px-3 btn-sm"><i class="fas fa-pen"></i></button> <button id="delete"  data-id="${id}" title="Delete" class="btn btn-outline-danger rounded px-3 btn-sm"><i class="fas fa-trash"></i></button>`]
     }
 
 
-    function refreshTable(data){
-        $('#productTable').DataTable().clear().draw();
-        data.forEach( (product,index) => {
-            let reqData = dataConverter(product,index+1)
-            const oTable = $('#productTable').dataTable();
-            oTable.fnAddData(reqData);
-        });
-    }
 
-
-    // function hit api and to get all products and add them to the jquery datatable
-    function getProducts() {
+    // function hit api and to get all stores and add them to the jquery datatable
+    async function getStores() {
         $.ajax({
-            url: server + '/products',
+            url: server + '/stores',
             type: 'GET',
             success: function (data) {
-
                 console.log(data); 
-
-                refreshTable(data);
                 // let myData = []
-                
-                // $('#productTable').DataTable().clear().draw();
-                // $('#productTable').DataTable().rows.add(myData).draw();
+                $('#storeTable').DataTable().clear().draw();
+                data.forEach( (store,index) => {
+                    let reqData = dataConverter(store,index+1)
+                    const oTable = $('#storeTable').dataTable();
+                    oTable.fnAddData(reqData);
+                });
+                // $('#storeTable').DataTable().clear().draw();
+                // $('#storeTable').DataTable().rows.add(myData).draw();
             },
             error: function (error) {
                 console.log(error);
@@ -48,7 +41,7 @@ $(document).ready(function () {
    
 
 
-    // add on click event listner on element with id as "add"
+    // click event listner on "Add Store" button to open add form
     $(document).on('click', '#add', function () {
         // open modal
         $('#addModal').modal('show');
@@ -59,7 +52,7 @@ $(document).ready(function () {
             success: function (data) {
                 console.log(data);
                 data.forEach(store => {
-                    $('#store_id').append(`<option class="getStoreProducts" value=${store.id}>${store.storeName}</option>`);
+                    $('#store_id').append(`<option value=${store.id}>${store.storeName}</option>`);
                 });
             },
             error: function (error) {
@@ -69,35 +62,34 @@ $(document).ready(function () {
     });
 
 
-    // on click ".getStoreProducts" hit api "/store/products/{store.id}" and refresh table
-
-    
-
-
-    // add on click event listner on element with id as "save"
+    // addForm Submit
     $(document).on('submit', '#addForm', function (e) {
         // prevent default  
         e.preventDefault();
         console.log(e.target)
         // replace text of button with spinner
-        $('#saveProduct').html('<span class="spinner-border spinner-border-sm" style="margin: 0px 34.87px;" role="status" aria-hidden="true"></span>');
+        $('#saveStore').html('<span class="spinner-border spinner-border-sm" style="margin: 0px 34.87px;" role="status" aria-hidden="true"></span>');
 
         //  get formdata
         var formData = new FormData(document.getElementById('addForm'));
 
-        // add product
+        var json = JSON.stringify(Object.fromEntries(formData));
+
+        console.log("JSON = " + json);
+
+        // add store
         $.ajax({
-            url: server + '/products',
+            url: server + '/stores',
             type: 'POST',
-            data: JSON.stringify(Object.fromEntries(formData)),
+            data: json,
             dataType: 'json',
             contentType: 'application/json',
             success: function (data) {
-                // get all products
-                getProducts();
+                // get all stores
+                getStores();
                 
                 // replace spinner with text
-                $('#saveProduct').html('Add Product');
+                $('#saveStore').html('Add Store');
                 
                 // close modal
                 $('#addModal').modal('hide');
@@ -109,7 +101,7 @@ $(document).ready(function () {
                 console.log(error);
 
                 // replace spinner with text
-                $('#saveProduct').html('Add Product');
+                $('#saveStore').html('Add Store');
                 
                 // close modal
                 $('#addModal').modal('hide');
@@ -126,14 +118,10 @@ $(document).ready(function () {
     // add on click event listner on element with id as "edit"
     $(document).on('click', '#edit', function () {
         var id = $(this).attr('data-id');
-        // copy details of product from table row to modal
-        // $('#editModal').find('#').val(id);
-        $('#editModal').find('#name').val($(this).closest('tr').find('td:eq(1)').text());
-        $('#editModal').find('#category').val($(this).closest('tr').find('td:eq(2)').text());
-        $('#editModal').find('#price').val($(this).closest('tr').find('td:eq(3)').text());
-        $('#editModal').find('#rating').val($(this).closest('tr').find('td:eq(5)').text());
-        $('#editModal').find('#quantity').val($(this).closest('tr').find('td:eq(4)').text());
-        $('#editModal').find('#description').val($(this).closest('tr').find('td:eq(4)').text());
+        // copy details of store from table row to modal
+        $('#editModal').find('#sid').val(id);
+        $('#editModal').find('#storeName').val($(this).closest('tr').find('td').eq(1).text());
+        $('#editModal').find('#storeAddress').val($(this).closest('tr').find('td').eq(2).text());
 
         // open modal
         $('#editModal').modal('show');
@@ -146,25 +134,21 @@ $(document).ready(function () {
         // replace text of button with spinner
         $('#update').html('<span class="spinner-border spinner-border-sm" style="margin: 0px 38.15px;" role="status" aria-hidden="true"></span>');
 
-        // get data from form
-
-        // var id = $('#editModal').find('#pid').val();
-        // var name = $('#editModal').find('#name').val();
-        // var category = $('#editModal').find('#category').val();
-        // var price = $('#editModal').find('#price').val();
-        // var quantity = $('#editModal').find('#quantity').val();
-
         var formData = new FormData(document.getElementById('addForm'));
 
-        // update product
+        var json = JSON.stringify(Object.fromEntries(formData));
+
+        console.log("JSON = " + json);
+
+        // update store
         $.ajax({
-            url: server + '/products',
+            url: server + '/stores',
             type: 'PUT',
-            data: JSON.stringify(Object.fromEntries(formData)),
+            data: json,
             contentType: 'application/json',
             success: function (data) {
-                // get all products
-                getProducts();
+                // get all stores
+                getStores();
 
                 // replace spinner with text
                 $('#update').html('Save changes');
@@ -201,13 +185,13 @@ $(document).ready(function () {
         // Replace text of button with spinner
         $target.html('<span class="spinner-border spinner-border-sm" style="width: 17px; margin: 1px -2.4px; height: 17px; vertical-align: sub;" role="status" aria-hidden="true"></span>');
 
-        // delete product
+        // delete store
         $.ajax({
-            url: server + '/products/' + id,
+            url: server + '/stores/' + id,
             type: 'DELETE',
             success: function (data) {
-                // get all products
-                getProducts();
+                // get all stores
+                getStores();
                 console.log(data)
                 // replace spinner with text
                 $target.html('<i class="fas fa-trash"></i>');
@@ -219,19 +203,4 @@ $(document).ready(function () {
             }
         });
     });
-});
-
-// hit api to get stores and append to select field with id "store_id"
-$.ajax({
-    url: server + '/store/stores',
-    type: 'GET',
-    success: function (data) {
-        console.log(data);
-        data.forEach(store => {
-            $('#storeFilter').append(`<option value=${store.id}>${store.storeName}</option>`);
-        });
-    },
-    error: function (error) {
-        console.log(error);
-    }
 });
